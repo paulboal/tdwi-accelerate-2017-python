7da37fa1701fed7ea6bd56e69ff39759a807056e
Paul Boal <paul.boal@amitechsolutions.com>
2017-09-29 14:37:52 -0500

    added zip files

--- CensusLookup.py
+++ CensusLookup.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 class CensusLookup:
-    def __init__(self, file='DEC_10_DP_DPDP1_with_ann.csv'):
+    def __init__(self, file='DEC_10_DP_DPDP1_with_ann.csv.zip'):
         self._file = 'DEC_10_DP_DPDP1_with_ann.csv'
         df = pd.read_csv(file, skiprows=1)
         df = df.set_index(df.Geography.apply(lambda x: x[6:]))
