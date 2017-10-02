30ca28d435be7729a56cf183d7e1388d4b1abf83
Paul Boal <paul.boal@amitechsolutions.com>
2017-10-02 16:45:48 -0500

    most of presentation done

--- CensusLookup.py
+++ CensusLookup.py
@@ -4,7 +4,7 @@ class CensusLookup:
     def __init__(self, file='DEC_10_DP_DPDP1_with_ann.csv.zip'):
         self._file = 'DEC_10_DP_DPDP1_with_ann.csv'
         df = pd.read_csv(file, skiprows=1)
-        df = df.set_index(df.Geography.apply(lambda x: x[6:]))
+        df = df.set_index(df['Geography'].apply(lambda x: x[6:]))
         self._df = df
 
     def get_data(self):
